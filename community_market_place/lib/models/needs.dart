043d89df55dc06e6a11c27d236ec7c04e0c5239a
Narayana <narayana.madineni1@ibm.com>
2020-06-27 23:35:05 +1000

    Needs page showing cards

--- community_market_place/lib/models/needs.dart
+++ community_market_place/lib/models/needs.dart
@@ -1,12 +1,17 @@
 class Need {
   String imageUrl;
   String name;
+  String title;
+  String user;
+  String date;
+  String description;
+  int viewCount;
 
-  Need({this.imageUrl, this.name});
+  Need({this.imageUrl, this.name, this.title, this.user, this.date, this.viewCount, this.description});
 }
 
 List<Need> needs = [
-  Need(imageUrl: "images/pic1.jpg", name: "Restaurants"),
-  Need(imageUrl: "images/pic2.jpg", name: "Grocery Stores"),
-  Need(imageUrl: "images/pic3.jpg", name: "Park and Trails"),
+  Need(imageUrl: "images/pic1.jpg", name: "Restaurants", title: "Need room for quarantine", user: "Naresh", date: "23/06/2020", viewCount: 10, description: "I need extra room"),
+  Need(imageUrl: "images/pic1.jpg", name: "Restaurants", title: "Need room for quarantine", user: "Naresh", date: "23/06/2020", viewCount: 10, description: "I need extra room"),
+  Need(imageUrl: "images/pic1.jpg", name: "Restaurants", title: "Need room for quarantine", user: "Naresh", date: "23/06/2020", viewCount: 10, description: "I need extra room"),
 ];
--- community_market_place/lib/screens/needs_screen.dart
+++ community_market_place/lib/screens/needs_screen.dart
@@ -1,15 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:communitymarketplace/models/needs.dart';
+
+
+class _PostDescription extends StatelessWidget {
+  const _PostDescription({
+    Key key,
+    this.title,
+    this.user,
+    this.date,
+    this.viewCount,
+  }) : super(key: key);
+
+  final String title;
+  final String user;
+  final String date;
+  final int viewCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 14.0,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
+          Text(
+            user,
+            style: const TextStyle(fontSize: 10.0),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
+          Text(
+            date,
+            style: const TextStyle(fontSize: 10.0),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
+          Text(
+            '$viewCount views',
+            style: const TextStyle(fontSize: 10.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class NeedListItem extends StatelessWidget {
+  const NeedListItem({
+    this.thumbnail,
+    this.title,
+    this.user,
+    this.date,
+    this.viewCount,
+  });
+
+  final Widget thumbnail;
+  final String title;
+  final String user;
+  final String date;
+  final int viewCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12.0))),
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: thumbnail,
+            ),
+            Expanded(
+              flex: 3,
+              child: _PostDescription(
+                title: title,
+                user: user,
+                date: date,
+                viewCount: viewCount,
+              ),
+            ),
+            const Icon(
+              Icons.more_vert,
+              size: 16.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 class Needs extends StatefulWidget {
   @override
   _NeedsState createState() => _NeedsState();
 }
 
+
 class _NeedsState extends State<Needs> {
   Widget build(BuildContext context) {
-    return Container(
-        height: 200,
-        width: 200,
-        child: Image(image: AssetImage('images/pic1.jpg')));
+    return GridView.builder(
+        itemCount: needs.length,
+        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1, childAspectRatio: 4),
+        itemBuilder: (BuildContext context, int index){
+          Need need = needs[index];
+          return NeedListItem(
+              user: need.user,
+              title: need.title,
+              date: need.date,
+              viewCount: need.viewCount,
+              thumbnail: Container(
+                height: 100,
+                child: Image(image: AssetImage(need.imageUrl))
+          ));
+        }
+    );
   }
 }
