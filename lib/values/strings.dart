183446dc289dfd422129594fa301674918691c23
rajeshwarg <rajaeshwar@gmail.com>
2020-06-27 19:28:19 +1000

    login screen check in

---
+++ lib/values/strings.dart
@@ -0,0 +1,15 @@
+part of values;
+
+class StringConst {
+
+  static const String REGISTER = "Register";
+  static const String LOGIN = "Login";
+  static const String SIGN_IN = "Sign in";
+  static const String SIGN_UP = "Sign up";
+  static const String USER_NAME = "Username";
+  static const String EMAIL_ADDRESS = "Email Address";
+  static const String PASSWORD = "Password";
+  static const String CONFIRM_PASSWORD = "Confirm Password";
+  static const String FORGOT_PASSWORD = "Forgot Password?";
+
+}
---
+++ lib/values/values.dart
@@ -0,0 +1,12 @@
+library values;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+part 'colors.dart';
+part 'borders.dart';
+part 'sizes.dart';
+part 'strings.dart';
+part 'styles.dart';
+part 'gradients.dart';
