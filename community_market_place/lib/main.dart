5df0e4351269c3001d398e33a4999b444259efd9
Naresh Idiga <naresh.maxreader@gmail.com>
2020-06-28 14:36:31 +0530

    Change app title

--- community_market_place/lib/main.dart
+++ community_market_place/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Community Market',
+      title: 'Community Share',
       theme: ThemeData(
         primarySwatch: Colors.orange,
         accentColor: Colors.blue,
