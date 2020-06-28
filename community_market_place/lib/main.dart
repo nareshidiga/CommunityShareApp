b8faf17163cce7b6fa7b49c1456ca04e9fe85bde
Naresh Idiga <naresh.maxreader@gmail.com>
2020-06-28 16:58:47 +0530

    Commit remove debug banner

--- community_market_place/lib/main.dart
+++ community_market_place/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Community Share',
       theme: ThemeData(
         primarySwatch: Colors.orange,
